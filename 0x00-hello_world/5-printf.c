45a6db471ac157ca825183eb949915d526cdbdbb
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-05 18:31:03 -0700

    aded prinf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using printf function to print
+ * a null terminated stream of chars.
+ * Return: 0 at success.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
