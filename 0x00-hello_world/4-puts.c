3733fa42cc65bac5940a7ee8845a9d67c2276f80
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-05 18:23:38 -0700

    added puts file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A script that puts a null terminated chars stream.
+ * Return:  0 at success.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
