476aa9b39df8ec736778cd977f6841ee3ea6b58e
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-10 12:31:56 -0700

    adding first file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: printing _putchar and newline.
+ * Return: 0 at success.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
