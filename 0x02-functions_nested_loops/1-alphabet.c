7a82790c1678c90bc2c9873afaa86f91d95779c3
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-10 17:23:58 -0700

    correct the file one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet: it does what exactly says.
+ *
+ * Description: it prints a list of english alphabets.
+ * Retrun: void.
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while(alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
