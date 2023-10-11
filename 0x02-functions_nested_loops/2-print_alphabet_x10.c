0bce126b9171f7800d4846a186c5d7556c5ccfbc
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-10 17:45:35 -0700

    adding the 3rd file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alpha 10 times.
+ *
+ * Description: prints the whole alphabet 10 times in a row..
+ * Return : void.
+ */
+void print_alphabet_x10(void)
+{
+	short int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
