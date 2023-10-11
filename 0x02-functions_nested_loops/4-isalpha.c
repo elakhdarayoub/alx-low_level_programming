5a8cc74bbb11b1544ea6481d007c111d97411393
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-10 18:29:32 -0700

    added 5th file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Does as name implies.
+ * @c: The character to check.
+ *
+ * Description: Checks wether a character is alphabet or not.
+ * Return: 1 if alpha, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if (_islower(c) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
