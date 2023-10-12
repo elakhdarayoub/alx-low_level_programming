cb03460896c3527bce7101d882b096337a81ccfd
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-11 17:16:42 -0700

    file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @n: The first integer.
+ * @m: The second integer.
+ *
+ * Description: Adds two integers.
+ * Return: Result.
+ */
+int add(int n, int m)
+{
+	return (m + n);
+}
