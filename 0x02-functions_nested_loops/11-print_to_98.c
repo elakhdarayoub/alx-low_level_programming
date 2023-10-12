e51d1f4615198295b9d2f0165f6f16d08fd65a84
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-11 19:09:13 -0700

    file 12 being fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,29 @@
  */
 void print_to_98(int n)
 {
-	unsigned short int i;
+	short int i;
 
-	for (i = n; i <= 98; i++)
+	if (n > 98)
 	{
-		if (i == 98)
-			printf("%d ", i);
-		else
-			printf("%d, ", i);
+		for (i = n; i >= 98; i--)
+		{
+
+			if (i == 98)
+				printf("%d ", i);
+			else
+				printf("%d, ", i);
+		}
+	}
+	else
+	{
+
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+				printf("%d ", i);
+			else
+				printf("%d, ", i);
+		}
 	}
 	putchar('\n');
 }
