90ac5272734bdc04931379a5c3bbaad36f8fade2
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-13 18:59:01 -0700

    optimized file 8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,12 +18,9 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			if (n != 1)
+			for (j = 0; j < i; j++)
 			{
-				for (j = 0; j < i; j++)
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
 			}
 			_putchar('\\');
 
