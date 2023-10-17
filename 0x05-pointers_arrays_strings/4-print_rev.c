fef7a441d59ae56eb92e8e4f636043c731ad1fe1
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-16 18:45:08 -0700

    segfult

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,13 @@
  */
 void print_rev(char *s)
 {
-	short int len;
+	int len;
 
 	len = 0;
 	while (s[len] != '\0')
 		len++;
-	len--; /*removing the extra char that rep '\0'*/
+
+	len--;
 	while (len >= 0)
 	{
 		_putchar(s[len]);
