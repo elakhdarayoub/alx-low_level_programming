dd8969dd4384fd2c0ca5c9e39caa062867d58e9f
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-16 17:40:29 -0700

    initial commit and file 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Reset any int value to 98.
+ * @n: The integer to reset.
+ *
+ * Description: Reseting the value of n.
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
