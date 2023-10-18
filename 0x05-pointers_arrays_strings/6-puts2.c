f0bb05f2c328da1fe0ae763845593b592bad585c
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-17 17:08:30 -0700

    file 7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other char.
+ * @str: The string.
+ *
+ * Description: Printing every other characher followed by newline.
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	short int i, j;
+
+	i = j = 0;
+	/* Calculate the length of the string*/
+	while (str[i] != '\0')
+		i++;
+
+	/* Printing the chars */
+	while (j <= i)
+	{
+		if (j % 2 == 0)
+			_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
