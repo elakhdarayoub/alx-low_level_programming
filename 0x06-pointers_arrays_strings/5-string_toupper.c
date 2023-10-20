28d986fd00ea2ddda484bbc906b6055f94b6b731
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-19 18:27:21 -0700

    file 6

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,19 @@
 #incude "main.h"
 
 /**
- * string_toupper - convert string to uppercase.
+ * *string_toupper - convert string to uppercase.
  * @str: The array.
  *
- * Description: Converts a string to uppercase.
  * Return: char array.
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-		;
-
-	for (j = 0; j < i; j++)
 	{
-		if (str[j] >= 98 && str[j] <= 122)
-			str[j] -= 32;
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
 	}
 	return (str)
 }
