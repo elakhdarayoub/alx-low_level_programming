97152d39a01532a968d093de2da4c9bf3c9d4821
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-20 19:41:33 -0700

    file 4 fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int len;
 
+	len = 0;
 	while (*s1)
 	{
 		if (*s1 != *s2)
 		{
-			len = ((int)*s1 - 48) - ((int)*s2 - 49);
+			len = ((int)*s1 - 48) - ((int)*s2 - 48);
 			break;
 		}
 		s1++;
