4623ee406757d28ca9bd8e85b7f3fadac32e2d3d
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-23 04:55:38 -0700

    first test of file 1 - memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - Fills memory with constant byte.
+ * @s: The pointer to memo area.
+ * @b: The constant byte.
+ * @n: The number of memory blocks to be filled.
+ *
+ * Description: Fills the memory area passed with the constant byte passed.
+ * Return: Pointer to the memory area.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	const c = b;
+
+	for (i = 0; i < n; i++)
+		s[i] = c;
+	return (s);
+}
