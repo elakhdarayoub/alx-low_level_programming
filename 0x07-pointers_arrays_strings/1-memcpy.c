80a910be453abb7b451c8d179c2c0be566c790e0
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-23 11:14:37 -0700

    file 2 modification

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,7 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	char *j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
 		j = dest;
 		*(j + i) = src[i];
