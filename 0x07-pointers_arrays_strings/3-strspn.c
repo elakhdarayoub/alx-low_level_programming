32ea0f279e35cac16864d47be7ec3a5b40f0ff76
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-24 12:16:26 -0700

    file 4 fixed

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,35 +1,6 @@
 #include "main.h"
 
 /**
- * string_type - Detects dynamically the type of bytes in string
- * @str: The string.
- * @range: The array containing accept range.
- *
- * Description: Checks the first element of str and assume that the whole
- * string bytes are in the same char class.
- * Return: void.
- */
-void string_type(char *str, int *range)
-{
-	/* For digits */
-	if (*str >= 48 && *str <= 57)
-	{
-		range[0] = 48;
-		range[1] = 57;
-	}
-	else if (*str >= 65 && *str <= 90)
-	{ /* For upper*/
-		range[0] = 65;
-		range[1] = 90;
-	}
-	else if (*str >= 97 && *str <= 122)
-	{/* For lower */
-		range[0] = 97;
-		range[1] = 122;
-	}
-}
-
-/**
  * _strspn - Gets the length of a prefix substring.
  * @s: The string.
  * @accept: The second string.
@@ -39,13 +10,16 @@ void string_type(char *str, int *range)
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n;
-	int range[2];
+	unsigned int i, j;
 
-	string_type(accept, range);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[j]; j++)
+		{
+			if (accept[j] == '\0')
+				return (j);
+		}
+	}
 
-	for (n = 0; (s[n] >= range[0] && s[n] <= range[1]); n++)
-		;
-
-	return (n);
+	return (j);
 }
