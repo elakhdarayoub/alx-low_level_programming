88e9792d3f3bf5632eb15217cf8335925fa2caaf
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-24 12:52:38 -0700

    file 7

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: Source string.
+ * @needle: The substring.
+ *
+ * Description: finds the first occurrence of a substring.
+ * Return: Array of chars.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	
+}
