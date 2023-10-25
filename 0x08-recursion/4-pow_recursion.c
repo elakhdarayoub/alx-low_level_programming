eecc8dc560951a8f16bb8a4f55452253fda1690d
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-25 12:24:37 -0700

    file 4 fixing

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,7 +12,9 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	if (y == 1)
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
 		return (x);
 	x *= _pow_recursion(x, --y);
 	return (x);
