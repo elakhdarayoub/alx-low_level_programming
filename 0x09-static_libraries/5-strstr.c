ce595b6274237593619aaafba0450e03ef8ad679
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-28 09:24:55 -0700

    please work

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,14 +1,1 @@
-#include "main.h"
-
-/**
- * _strstr - Locates a substring.
- * @haystack: Source string.
- * @needle: The substring.
- *
- * Description: finds the first occurrence of a substring.
- * Return: Array of chars.
- */
-char *_strstr(char *haystack, char *needle)
-{
-	
-}
+char *_strstr(char *haystack, char *needle);
