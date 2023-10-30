29ea22c4dadaeaf060e567dc7226dcc1b91142ee
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-10-30 13:09:38 -0700

    file 1 - fixed 2 problems out of 4

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 
 /**
  * _strdup - Dublicates a string in a newly allocated space.
- * str: The string to dublicate.
+ * @str: The string to dublicate.
  *
  * Description: Dublicates a string in a dynamically allocated space.
  * Return: Base address of a char array.
@@ -24,7 +24,7 @@ char *_strdup(char *str)
 	 * Allocating size of bytes for arr and
 	 * make sure the wanted space is available
 	 */
-	for (j = 0; j < size; j++)
+	for (j = 0; j <= size; j++)
 	{
 		arr = malloc(sizeof(*arr));
 		if (arr == NULL)
