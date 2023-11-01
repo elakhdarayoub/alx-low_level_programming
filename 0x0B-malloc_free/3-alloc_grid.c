d26e70abb33bd68db8161af40661d0a5d0742b18
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-01 16:13:58 -0700

    file 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	tab = malloc(sizeof(*tab) * height);
 
-	if (width <=  || height <= 0 || tab == 0)
+	if (width <= 0 || height <= 0 || tab == 0)
 	{
 		return (NULL);
 	}
