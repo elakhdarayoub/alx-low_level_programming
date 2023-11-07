e3d61c04792b64662f9ce81667a766847efc5c67
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-07 11:37:47 -0800

    file 0 linter fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct Dog - The structure name.
+ * struct dog - The structure name.
  * @name: Name of the instance.
  * @age: The age of the instance.
  * @owner: Name of the instance owner.
