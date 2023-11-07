0c50149aa39eb3aca5eb1cef21a7c94b206314e4
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-07 11:30:41 -0800

    file 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct Dog - The structure name.
+ * @name: Name of the instance.
+ * @age: The age of the instance.
+ * @owner: Name of the instance owner.
+ *
+ * Description: Creates a Dog instances.
+ */
+struct Dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
