e15301b45181ae8e3cecf38c77df2b1bd21f3ac2
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-07 12:07:52 -0800

    file 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,11 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *inst;
 
-	inst = d;
-
-	inst->name = name;
-	inst->age = age;
-	inst->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
