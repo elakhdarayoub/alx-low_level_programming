5097b4268c5f8002e592273490902116f3e16429
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-08 14:19:39 -0800

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Applies a function on array elements.
+ * @array: The array to operates on.
+ * @size: Size of the array.
+ * @action: A pointer to a function.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && action)
+	{
+		size_t i;
+
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,8 +2,10 @@
 #define FUNC_PTRS
 
 #include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
