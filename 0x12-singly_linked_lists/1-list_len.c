74628a9b207881d36fedd251827261e0de2f72b0
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-17 02:52:56 +0100

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * list_len - Gets the number of elements in a linked list.
+ * @h: Pointer to a list.
+ * Return: The length of a list.
+ */
+size_t list_len(const list_t *h)
+{
+	int elements = 1;
+
+	if (h->next != NULL)
+		elements += list_len(h->next);
+	return (elements);
+}
