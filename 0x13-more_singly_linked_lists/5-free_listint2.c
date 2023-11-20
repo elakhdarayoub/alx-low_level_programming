d33e6a8a6091ffb968bd7ffb3d5b223c5f1b1966
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-20 21:24:32 +0100

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a list again.
+ * @head: A double pointer to the list entry point.
+ *
+ * Description: Frees a list but sets its entry to NULL;
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+
+	current = *head;
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	free(current);
+	*head = NULL;
+}
