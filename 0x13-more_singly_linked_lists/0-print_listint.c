a951fa85936b7ef3fa9dba8c9fa45d0403693e78
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-21 00:24:27 +0100

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,12 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t count = 1;
 
 	if (h == NULL)
 		return (0);
+
 	printf("%d\n", h->n);
-	count++;
 	count += print_listint(h->next);
+
 	return (count);
 }
+
