65c9834965fd1f509e675ddd25139f4ca0c4beb2
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-20 20:17:46 +0100

    fixed an undetected bug in task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -28,7 +28,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		current = *head;
 		while (current->next != NULL)
-			current++;
+			current = current->next;
 		current->next = newNode;
 	}
 	return (current->next);
