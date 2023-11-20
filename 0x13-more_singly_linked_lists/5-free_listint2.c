3fb2144f16fc41020c459227bd673a0769e84376
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-21 00:52:44 +0100

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,12 +11,11 @@ void free_listint2(listint_t **head)
 	listint_t *current, *next;
 
 	current = *head;
-	while (current)
+	while (current != NULL)
 	{
 		next = current->next;
 		free(current);
 		current = next;
 	}
-	free(current);
 	*head = current;
 }
