bea8cbb375fdee3791d54e73b7eb54fa7313fc9c
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-21 23:14:24 +0100

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,6 +11,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current, *Node;
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
 	current = head;
 	for (i = 0; i <= index; i++)
 	{
