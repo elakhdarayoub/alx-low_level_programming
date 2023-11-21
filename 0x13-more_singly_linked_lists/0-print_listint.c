bb28e7ce25967213c605f41403ec1986550afea6
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-21 01:03:06 +0100

    -

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 /**
  * print_listint - Prints elements of a linked list.
  * @h: Entry point to the list.
- * Return: Number of nudes.
+ * Return: Number of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
