b56f33e1245f56319fd58ad2b9050374c8afb466
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-22 02:44:22 +0100

    task 10 - not fully working

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+ * listint_len - Prints the length of list.
+ * @h: Entry point to a list.
+ * Return: Number of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 1;
+
+	if (h == NULL)
+		return (0);
+	count += listint_len(h->next);
+	return (count);
+}
+
+/**
  * get_nodeint_at_index - Gets (index)nt node in a list.
  * @head: List entry point.
  * @index: The node index, starts at 0.
@@ -11,6 +26,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current, *Node;
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
 	current = head;
 	for (i = 0; i <= index; i++)
 	{
@@ -33,32 +50,36 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *previousNode = NULL, *delete;
+	listint_t *previousNode, *delete, *temp;
 
 	if (*head == NULL)
 		return (-1);
 
 	if (index == 0)
-		free(*head);
-	else if (index == 1)
 	{
-		delete = get_nodeint_at_index(*head, index);
-		if (delete)
-			free(delete);
+		if (!(*head)->next)
+			free(*head);
 		else
-			return (-1);
+		{
+			temp = (*head)->next;
+			free(*head);
+			*head = temp;
+		}
+		return (1);
 	}
-	else
+	else if (listint_len(*head) > index)
 	{
 		previousNode = get_nodeint_at_index(*head, index - 1);
-		if (previousNode)
-		{
-			delete = previousNode->next;
-			previousNode->next = delete->next;
-			free(delete);
-		}
-		else
-			return (-1);
+		delete = previousNode->next;
+		previousNode->next = delete->next;
+		free(delete);
+		return (1);
 	}
-	return (1);
+	else if (listint_len(*head) == index)
+	{
+		previousNode = get_nodeint_at_index(*head, index - 1);
+		free(previousNode->next);
+		return (1);
+	}
+	return (-1);
 }
