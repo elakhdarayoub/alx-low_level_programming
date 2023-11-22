da6adf9f1518031b7e6cdf16bf4a7fd8dbdb4485
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-22 23:02:51 +0100

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void print_before(void) __attribute__ ((constructor));
+
+/**
+ * print_before - Prints before main.
+ * Return: void.
+ */
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			" I bore my house upon my back!\n");
+}
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
