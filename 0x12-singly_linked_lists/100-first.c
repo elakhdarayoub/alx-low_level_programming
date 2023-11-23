08f106588b3c97612e150a78e3706eda7d939da5
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-23 01:19:13 +0100

    task 10

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,7 +9,7 @@ void print_before(void) __attribute__ ((constructor));
 void print_before(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
-			" I bore my house upon my back!\n");
+			"I bore my house upon my back!\n");
 }
 
 /**
