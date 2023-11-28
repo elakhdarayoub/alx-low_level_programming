ff4a09c3e462e981801a0e2995c6fd1e7d01af88
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-28 03:02:50 +0100

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <fcnlt.h>
+#include <fcntl.h>
 #include "main.h"
 
 /**
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWSUR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
